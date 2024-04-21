e66b02ebc052fa3334a8231bd206925ba4f0db27
Raynesz <31424806+Raynesz@users.noreply.github.com>
2024-04-22 02:05:56 +0300

    inventory entries use quality color

--- Source/L3/InventoryEntry.cpp
+++ Source/L3/InventoryEntry.cpp
@@ -15,7 +15,34 @@ void UInventoryEntry::NativeOnListItemObjectSet(UObject* ListItemObject)
 
 	UItemUIObject* UIItem = Cast<UItemUIObject>(ListItemObject);
 	ItemName->SetText(FText::FromString(UIItem->ItemData.ItemName.ToString()));
-	ItemLevel->SetText(FText::AsNumber(UIItem->ItemData.Level));
+
+	FLinearColor TextColor = FLinearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+	if (UIItem->ItemData.Quality == "Legendary")
+	{
+		TextColor = FLinearColor(1.0f, 0.5f, 0.0f, 1.0f);
+	}
+	else if (UIItem->ItemData.Quality == "Epic")
+	{
+		TextColor = FLinearColor(0.64f, 0.2f, 0.93f, 1.0f);
+	}
+	else if (UIItem->ItemData.Quality == "Rare")
+	{
+		TextColor = FLinearColor(0.0f, 0.44f, 0.87f, 1.0f);
+	}
+	else if (UIItem->ItemData.Quality == "Uncommon")
+	{
+		TextColor = FLinearColor(0.12f, 0.7f, 0.0f, 1.0f);
+	}
+	else if (UIItem->ItemData.Quality == "Poor")
+	{
+		TextColor = FLinearColor(0.5f, 0.5f, 0.5f, 1.0f);
+	}
+
+	ItemName->SetColorAndOpacity(TextColor);
+
+	ItemLevel->SetText(FText::FromString(FString::Printf(TEXT("[%d]"), UIItem->ItemData.Level)));
+	ItemLevel->SetColorAndOpacity(FLinearColor(1.0f, 1.0f, 0.0f, 1.0f));
 
 	DropButton->OnClicked.Clear();
 	DropButton->OnClicked.AddDynamic(this, &UInventoryEntry::DropItem);
