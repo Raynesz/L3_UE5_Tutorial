ab651eef96a8eca7f08b845440437d42eacb0984
Raynesz <31424806+Raynesz@users.noreply.github.com>
2024-04-22 16:02:06 +0300

    removed exception handling for shipping

--- Source/L3/ItemDatabase.cpp
+++ Source/L3/ItemDatabase.cpp
@@ -11,48 +11,40 @@ TArray<FItemData> UItemDatabase::Items;
 
 void UItemDatabase::LoadItemsFromJson()
 {
-    try {
-        FString JsonFilePath = FPaths::ProjectContentDir() + TEXT("Data/") + TEXT("L3_ItemDB.json");
+    FString JsonFilePath = FPaths::ProjectContentDir() + TEXT("Data/") + TEXT("L3_ItemDB.json");
 
-        FString JsonString;
-        if (!FFileHelper::LoadFileToString(JsonString, *JsonFilePath))
-        {
-            UE_LOG(LogTemp, Error, TEXT("Failed to load JSON file from %s"), *JsonFilePath);
-            return;
-        }
-
-        TArray<TSharedPtr<FJsonValue>> JsonArray;
-        TSharedRef<TJsonReader<>> JsonReader = TJsonReaderFactory<>::Create(JsonString);
-
-        if (!FJsonSerializer::Deserialize(JsonReader, JsonArray))
-        {
-            UE_LOG(LogTemp, Error, TEXT("Failed to parse JSON file"));
-            return;
-        }
-
-        for (const auto& ItemValue : JsonArray)
-        {
-            const TSharedPtr<FJsonObject> ItemObject = ItemValue->AsObject();
-            if (ItemObject.IsValid())
-            {
-                FItemData ItemData;
-                ItemData.ItemGuid = FGuid::NewGuid();
-                ItemData.ItemName = FName(*ItemObject->GetStringField(TEXT("Name")));
-                ItemData.Quality = ItemObject->GetStringField(TEXT("Quality"));
-                ItemData.Level = ItemObject->GetIntegerField(TEXT("Level"));
-                ItemData.ItemType = ItemObject->GetStringField(TEXT("IType"));
-                ItemData.UniqueEquipped = ItemObject->GetBoolField(TEXT("UniqueEquipped"));
-                ItemData.Mesh = ItemObject->GetStringField(TEXT("Mesh"));
-
-                Items.Add(ItemData);
-            }
-        }
+    FString JsonString;
+    if (!FFileHelper::LoadFileToString(JsonString, *JsonFilePath))
+    {
+        UE_LOG(LogTemp, Error, TEXT("Failed to load JSON file from %s"), *JsonFilePath);
+        return;
     }
-    catch (const std::exception& e) {
-        UE_LOG(LogTemp, Warning, TEXT("Standard library exception caught: %s"), UTF8_TO_TCHAR(e.what()));
+
+    TArray<TSharedPtr<FJsonValue>> JsonArray;
+    TSharedRef<TJsonReader<>> JsonReader = TJsonReaderFactory<>::Create(JsonString);
+
+    if (!FJsonSerializer::Deserialize(JsonReader, JsonArray))
+    {
+        UE_LOG(LogTemp, Error, TEXT("Failed to parse JSON file"));
+        return;
     }
-    catch (...) {
-        UE_LOG(LogTemp, Warning, TEXT("Unknown exception caught"));
+
+    for (const auto& ItemValue : JsonArray)
+    {
+        const TSharedPtr<FJsonObject> ItemObject = ItemValue->AsObject();
+        if (ItemObject.IsValid())
+        {
+            FItemData ItemData;
+            ItemData.ItemGuid = FGuid::NewGuid();
+            ItemData.ItemName = FName(*ItemObject->GetStringField(TEXT("Name")));
+            ItemData.Quality = ItemObject->GetStringField(TEXT("Quality"));
+            ItemData.Level = ItemObject->GetIntegerField(TEXT("Level"));
+            ItemData.ItemType = ItemObject->GetStringField(TEXT("IType"));
+            ItemData.UniqueEquipped = ItemObject->GetBoolField(TEXT("UniqueEquipped"));
+            ItemData.Mesh = ItemObject->GetStringField(TEXT("Mesh"));
+
+            Items.Add(ItemData);
+        }
     }
 }
 
--- Source/L3/L3GameMode.cpp
+++ Source/L3/L3GameMode.cpp
@@ -15,32 +15,14 @@ void AL3GameMode::BeginPlay()
 {
     Super::BeginPlay();
 
-    UE_LOG(LogTemp, Warning, TEXT("BeginPlay"));
+	UItemDatabase::LoadItemsFromJson();
 
-	try {
-		UItemDatabase::LoadItemsFromJson();
-
-		for (const FItemData& Item : UItemDatabase::Items)
-		{
-			AItem* SpawnedItem = GetWorld()->SpawnActor<AItem>(AItem::StaticClass(), FVector(0.f, 0.f, 200.f), FRotator());
-			if (SpawnedItem) {
-				SpawnedItem->SetItemProperties(Item.ItemGuid, Item.Quality, Item.Mesh);
-			}
-
-			UE_LOG(LogTemp, Log, TEXT("ItemGuid: %s"), *Item.ItemGuid.ToString());
-			UE_LOG(LogTemp, Warning, TEXT("ItemName: %s"), *Item.ItemName.ToString());
-			UE_LOG(LogTemp, Warning, TEXT("Quality: %s"), *Item.Quality);
-			UE_LOG(LogTemp, Warning, TEXT("Level: %d"), Item.Level);
-			UE_LOG(LogTemp, Warning, TEXT("ItemType: %s"), *Item.ItemType);
-			UE_LOG(LogTemp, Warning, TEXT("UniqueEquipped: %s"), Item.UniqueEquipped ? TEXT("True") : TEXT("False"));
-			UE_LOG(LogTemp, Warning, TEXT("Mesh: %s"), *Item.Mesh);
+	for (const FItemData& Item : UItemDatabase::Items)
+	{
+		AItem* SpawnedItem = GetWorld()->SpawnActor<AItem>(AItem::StaticClass(), FVector(0.f, 0.f, 200.f), FRotator());
+		if (SpawnedItem) {
+			SpawnedItem->SetItemProperties(Item.ItemGuid, Item.Quality, Item.Mesh);
 		}
-	}
-	catch (const std::exception& e) {
-		UE_LOG(LogTemp, Warning, TEXT("Standard library exception caught: %s"), UTF8_TO_TCHAR(e.what()));
-	}
-	catch (...) {
-		UE_LOG(LogTemp, Warning, TEXT("Unknown exception caught"));
 	}
 }
 
